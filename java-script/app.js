bac0ceb2a45a297e1be61eecdb630888eeae9724
Akhand-Jyoti-Raj <rahulkumar108642@gmail.com>
2024-12-21 22:30:50 +0530

    puch

---
+++ java-script/app.js
@@ -0,0 +1,15 @@
+console.log("Hello world !");
+console.log("Akhand is the best man in the world !");
+
+console.log("  ")
+
+let age = 17;
+
+if (age >= 18) {
+    console.log("You can vote ");
+
+} else {
+    console.log("You can't vote ");
+}
+
+console.log("End of page here -------------->>>>>>>>>>>>>")
